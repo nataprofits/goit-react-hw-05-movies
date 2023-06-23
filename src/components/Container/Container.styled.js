46999e202cda6105c5676850cff94bedf361ebed
Tkachenko Natalia <nataprofits@gmail.com>
2023-06-23 15:29:51 +0100

    app

--- src/components/Container/Container.styled.js
+++ src/components/Container/Container.styled.js
@@ -3,6 +3,6 @@ import styled from '@emotion/styled';
 
 export const Container = styled.div`
   padding-right: 30px;
-  padding-left: 30px;
+  /* padding-left: 15px; */
   background: radial-gradient(circle, rgba(63,94,251,1) 0%, rgba(128,109,113,1) 100%);
 `;
